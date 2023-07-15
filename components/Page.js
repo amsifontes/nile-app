3a41649e51d86348c57b878a591a2ca9b1fd51f5
Anthony Sifontes <anthony.sifontes@gmail.com>
2023-07-14 23:19:58 -0500

    Add global style.

--- components/Page.js
+++ components/Page.js
@@ -1,9 +1,25 @@
 import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+html {
+    --red: #ff0000;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offwhite: #ededed;
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0, 0, 0, 0.9);
+}
+`;
 
 export default function Page({ children, cool }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
       <h2>page component</h2>
       {children}
