9f4ed540397ff500c3f4320d2621cef03d306b5c
Anthony Sifontes <anthony.sifontes@gmail.com>
2023-07-14 23:12:12 -0500

    Add index page.

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Page from '../components/Page';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <p>Hello there, traveller of the Nile!</p>
+      <p>Welcome to the index page</p>
+    </div>
+  );
+}
